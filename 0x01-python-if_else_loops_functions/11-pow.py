79ece93bd0186de156fab07f725e2749d6bef69e
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-08 21:37:23 +0300

    fix

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    return (a ** b)
+def pow(a, b):
+    result = 1
+    if b < 0:
+        a = 1 / a
+        b = -b
+    for _ in range(b):
+        result *= a
+    return result
