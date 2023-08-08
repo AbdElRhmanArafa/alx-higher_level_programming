b4cb19ef9177f4704aaab0a5242062b2c33a5533
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-08 21:52:17 +0300

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    output = []
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            output.append("FizzBuzz")
+        elif number % 3 == 0:
+            output.append("Fizz")
+        elif number % 5 == 0:
+            output.append("Buzz")
+        else:
+            output.append(str(number))
+    print(" ".join(output))
