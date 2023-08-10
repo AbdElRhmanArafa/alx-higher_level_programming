983189ce532f7a2821706761d36a0b687380cc3c
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-11 00:25:34 +0300

    add

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    operator = ['+', '-', '*', '/']
+    result = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
+    for index, i in enumerate(operator):
+        print("{:d} {:s} {:d} = {:d}".format(a, i, b, result[index]))
+  
