b2e1f1b75561a226cd80f59941a3efa7026d789c
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-11 00:26:48 +0300

    fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     result = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
     for index, i in enumerate(operator):
         print("{:d} {:s} {:d} = {:d}".format(a, i, b, result[index]))
-  
