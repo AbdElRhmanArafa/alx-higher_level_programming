2985d26cfb887d86aa4e6eb26d368b523a6014d7
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-11 15:11:45 +0300

    ad 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+arg_list = sys.argv
+
+num_arguments = len(arg_list) - 1
+
+if num_arguments == 0:
+    print("0 arguments.")
+elif num_arguments == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(num_arguments))
+
+for index, value in enumerate(arg_list[1:], start=1):
+    print("{}: {}".format(index, value))
