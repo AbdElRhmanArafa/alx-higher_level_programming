05197348d87f6e144b22615448ab85c22dd30c09
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-11 15:19:12 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import sys
-arg_list = sys.argv
+def priny_args():
+    arg_list = sys.argv
 
-num_arguments = len(arg_list) - 1
+    num_arguments = len(arg_list) - 1
 
-if num_arguments == 0:
-    print("0 arguments.")
-elif num_arguments == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(num_arguments))
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_arguments))
 
-for index, value in enumerate(arg_list[1:], start=1):
-    print("{}: {}".format(index, value))
+    for index, value in enumerate(arg_list[1:], start=1):
+        print("{}: {}".format(index, value))
+if __name__ == "__main__":
+    priny_args()
