260f9ba5313fef6cf3a083ae38bf53c3c13fcac4
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-13 15:28:42 +0300

    -new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    for index, value in enumerate(my_list):
+        if index == idx:
+            new_list.append(element)
+        else:
+            new_list.append(value)
+    return new_list
