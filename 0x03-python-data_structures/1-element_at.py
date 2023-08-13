a731881abe6a84b02720d1159c0d601f7faa9ad2
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-13 11:57:38 +0300

    Secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if 0 <= idx < len(idx):
+        return my_list[idx]
+    else:
+        return "None"
+    
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4]
+    index = 2
+    result = element_at(my_list, index)
+    if result is not None:
+        print(result)
+    else:
+        print("Index out of range")
