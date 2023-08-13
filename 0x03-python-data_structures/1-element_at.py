98a82a1134f8bd8248310bacbac1098083c9f435
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-13 12:21:50 +0300

    fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,12 +4,3 @@ def element_at(my_list, idx):
         return my_list[idx]
     else:
         return "None"
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4]
-    index = 2
-    result = element_at(my_list, index)
-    if result is not None:
-        print(result)
-    else:
-        print("Index out of range")
