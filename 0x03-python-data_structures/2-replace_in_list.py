e43e064cb5a6b2a778fdec71f4a8357b19b66f27
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-13 12:46:58 +0300

    fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_lsit, idx, element):
-    if 0 <= idx <= len(my_lsit):
+    if 0 <= idx < len(my_lsit):
         my_lsit[idx] = element
     else:
-        return my_lsit
-
+        return(my_lsit)
