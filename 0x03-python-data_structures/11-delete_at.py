31769f504278c75679b74b9793d391673e4b6997
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-13 20:32:49 +0300

    add

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if 0 <= idx < len(my_list):
+        temp_list = []
+        for i in range(len(my_list)):
+            if i == idx:
+                continue
+            temp_list += my_list[i]
+    return my_list
