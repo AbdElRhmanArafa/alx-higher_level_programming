53160970e9f7ce2274d3fe4264c5d8335d387cb5
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-13 17:10:33 +0300

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
-    newT = len(sentence), sentence[0]
+        newT = len(sentence), "None"
+    else:
+        newT = len(sentence), sentence[0]
     return newT
