f7845531a855e58cfbc99e48c2d2573bc8ba3db3
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-08-18 01:36:36 +0300

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,11 @@
 def roman_to_int(roman_string):
     Latin = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
-    index = 0
-    for Rome in roman_string:
-        if index < len(roman_string) - 1:
-            if Rome == 'I' and roman_string[index + 1] == 'X':
-                sum -= 2
-        sum += Latin[Rome]
-        index += 1
+    pre_value = 0
+    for Rome in reversed(roman_string):
+        if Latin[Rome] >= pre_value:
+            sum += Latin[Rome]
+        else:
+            sum -= Latin[Rome]
+        pre_value = Latin[Rome]
     return sum
