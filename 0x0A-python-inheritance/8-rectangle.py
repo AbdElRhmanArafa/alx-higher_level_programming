7d9bdb9a8c306dff3185242178bb707ea3984740
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-09-12 23:54:58 +0300

    add

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry."""
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
