213b6da025d69089c80d1f6173f638e2c43bca5d
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-09-12 21:55:16 +0300

    mylist

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/python3
-"""A custom list class that inherits from the built-in list class."""
-
-class MyList(list):
-    """print sorted list"""
-    def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
