1cb193c523290abfeb069eb82cc861baa20dac50
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-09-14 04:05:17 +0300

    fix var

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,10 +4,11 @@
 
 class Student:
     """Student class"""
+
     def __init__(self, first_name, last_name, age):
         """constructor"""
         self.first_name = first_name
-        self.lastname = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
