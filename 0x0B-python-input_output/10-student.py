735744e6b67f3d3fa2dc5d94bc7e25b7838447ea
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-09-14 04:12:17 +0300

    Student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """convert to json
+        # Create a dictionary comprehension that iterates through the attributes
+        # listed in attrs and includes them in the result only if they exist in the object.
+        """
+        if type(attrs) == list and all(type(val) == str for val in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
